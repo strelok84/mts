30a94f1b719065265d085ec96b353110c88756a4
Andrey Bespalov <streloc84@gmail.com>
2021-04-13 13:22:58 +0400

    form

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,12 +36,76 @@ class Form extends React.Component {
       .catch((error) => console.log("error", error));
   }
 
+  searchName = (event) => {
+    this.setState((prevState) => ({
+      address: event.target.value,
+    }));
+    this.componentDidMount();
+    const details = document.getElementsByClassName("form_address")[0];
+    const map = document.getElementsByClassName("map");
+    const ad1 = details.children[1];
+    details.open = true;
+    /* map[0].style.gridRow="3/4"
+    details.style.gridRow="2/3" */
+  };
+
+  chosenName = (data, event) => {
+    //const latlng=document.getElementsByClassName("latlng")
+    if (data.data.house) {
+      this.setState({
+        lat:+data.data.geo_lat,
+        lng:+data.data.geo_lon,
+        
+      })
+      alert(`Широта: ${this.state.lat} Долгота: ${this.state.lng}`)
+      
+    }
+    const address = document.getElementById("client_address");
+    address.value = event.target.innerHTML;
+    const details = document.getElementsByClassName("form_address")[0];
+    details.open = false;
+
+    /* const map = document.getElementsByClassName("map")
+    map[0].style.gridRow="2/3"
+    details.style.gridRow="3/4" */
+  };
+
   render() {
     return (
-    <form className="form">
-        <input></input>
-    </form>
-    )
+      <form className="form">
+        <h4 className="form_head">Оставьте заявку на подключение</h4>
+        <p className="flat">
+          <input type="radio" name="place"></input> В квартиру
+        </p>
+        <p className="house">
+          <input type="radio" name="place"></input> В офис/частный дом
+        </p>
+        <input type="text" placeholder="Фамилия" className="sername"></input>
+        <input type="text" placeholder="Имя" className="client_name"></input>
+        <input
+          type="text"
+          placeholder="Отчество"
+          className="father_name"
+        ></input>
+        <input type="text" placeholder="Ваш телефон" className="phone"></input>
+        <input
+          type="text"
+          placeholder="Ваш адрес"
+          id="client_address"
+          className="client_address"
+          onChange={this.searchName}
+        ></input>
+        <details className="form_address">
+          <summary></summary>
+          <div onClick={(event) => this.chosenName(this.state.data[0], event)}>
+            {this.state.data[0]
+              ? this.state.data[0].value
+              : "Неизвестный адрес"}
+          </div>
+        </details>
+        <input type="submit" className="submit"></input>
+      </form>
+    );
   }
 }
 
